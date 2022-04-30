4fce30089c2cf010fcdbb126e11ad6da7a6c3002
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-04-30 17:59:04 -0300

    feat: move login form inside Login page and add page style

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,7 +1,14 @@
 import * as React from 'react';
 
+/* Children */
+import LoginForm from '../components';
+
 function Login() {
-  return <section id="login">Hello World!</section>;
+  return (
+    <section id="login" className="FullCenterContainer">
+      <LoginForm />
+    </section>
+  );
 }
 
 export default Login;
