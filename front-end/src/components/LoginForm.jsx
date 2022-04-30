dd014f08eb1f5c568f9a019de5fe798ba1ddde6d
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-04-30 15:16:25 -0300

    feat: implement first half of LoginForm

--- front-end/src/components/LoginForm.jsx
+++ front-end/src/components/LoginForm.jsx
@@ -1,7 +1,31 @@
 import * as React from 'react';
 
 function LoginForm() {
-  return <form id="login-form" action="" />;
+  return (
+    <form id="login-form" action="">
+      <input
+        id="login-email-input"
+        type="text"
+        data-testid="common_login__input-email"
+      />
+
+      <input
+        id="login-password-input"
+        type="text"
+        data-testid="common_login__input-password"
+      />
+
+      <button id="login-btn" type="submit" data-testid="common_login__button-login">
+        Login
+      </button>
+
+      <button id="sign-in-btn" type="button" data-testid="common_login__button-register">
+        <a href="https://">
+          Sign In
+        </a>
+      </button>
+    </form>
+  );
 }
 
 export default LoginForm;
