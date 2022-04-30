f44c5222e408f49e603f228d2a4d4a4bb7b5d189
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-04-30 16:03:41 -0300

    test: write second half of LoginForm test

--- front-end/src/tests/LoginForm.test.js
+++ front-end/src/tests/LoginForm.test.js
@@ -5,7 +5,7 @@ import React from 'react';
 import '@testing-library/jest-dom';
 
 // import react-testing methods
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 
 // the component to test
 import LoginForm from '../components';
@@ -17,10 +17,26 @@ describe('Login Form Component', () => {
     expect(emailInput).toBeInTheDocument();
   });
 
+  test('It is possible to change the email input value', () => {
+    render(<LoginForm />);
+    const emailInput = screen.getByTestId('common_login__input-email');
+    const exampleEmail = 'example@mail.com';
+    fireEvent.change(emailInput, { target: { value: exampleEmail } });
+    expect(emailInput.value).toBe(exampleEmail);
+  });
+
   test('Login password input is present in the document', () => {
     render(<LoginForm />);
     const passwordInput = screen.getByTestId('common_login__input-password');
     expect(passwordInput).toBeInTheDocument();
+  });
+
+  test('It is possible to change the password input value', () => {
+    render(<LoginForm />);
+    const passwordInput = screen.getByTestId('common_login__input-password');
+    const examplePassword = 'example@mail.com';
+    fireEvent.change(passwordInput, { target: { value: examplePassword } });
+    expect(passwordInput.value).toBe(examplePassword);
   });
 
   test('Login button is present in the document', () => {
@@ -29,9 +45,34 @@ describe('Login Form Component', () => {
     expect(loginButton).toBeInTheDocument();
   });
 
-  test('Sign-in button is present in the document', () => {
+  test('Login button has the correct text', () => {
     render(<LoginForm />);
-    const signInButton = screen.getByTestId('common_login__button-register');
-    expect(signInButton).toBeInTheDocument();
+    const loginButton = screen.getByTestId('common_login__button-login');
+    expect(loginButton).toHaveTextContent(/log in$/i);
+  });
+
+  test('Sign up button is present in the document', () => {
+    render(<LoginForm />);
+    const signUpButton = screen.getByTestId('common_login__button-register');
+    expect(signUpButton).toBeInTheDocument();
+  });
+
+  test('Sign up button has the correct text', () => {
+    render(<LoginForm />);
+    const signUpButton = screen.getByTestId('common_login__button-register');
+    expect(signUpButton).toHaveTextContent(/sign up$/i);
+  });
+
+  describe('When the email input has an invalid value', () => {
+    test('Invalid email error message is present in the document', () => {
+      render(<LoginForm />);
+      const emailInput = screen.getByTestId('common_login__input-email');
+      fireEvent.change(emailInput, { target: { value: 'abc' } });
+      fireEvent.focusOut(emailInput);
+      const errMessage = screen.getByTestId(
+        'common_login__element-invalid-email',
+      );
+      expect(errMessage).toBeInTheDocument();
+    });
   });
 });
