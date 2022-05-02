74ecb631017619223154dd1a84b148680a00f1da
Emanoel Mattos <emanoelmattos13@gmail.com>
2022-05-02 18:01:03 -0300

    feat: implements JWT secret key access

--- back-end/src/utils/JwtMethods.js
+++ back-end/src/utils/JwtMethods.js
@@ -1,6 +1,8 @@
 const jwt =  require('jsonwebtoken');
 const fs =  require('fs');
 
+const JWT_SECRET = fs.readFileSync('./jwt.evaluation.key', 'utf-8');
+
 class JwtMethods {
   static jwtSign(payload) {
     const sign = jwt.sign(payload, JWT_SECRET, {
