881e01bd51c2448db5f4fa8a09c9bff01fc47dc5
Emanoel Mattos <emanoelmattos13@gmail.com>
2022-05-02 20:06:24 -0300

    feat: create encryptPass method

---
+++ back-end/src/utils/HashPassMethods.js
@@ -0,0 +1,13 @@
+// https://gist.github.com/kitek/1579117
+const crypto = require('crypto');
+
+
+class HashPassMethods {
+  static encryptPass(password) {
+    const encrypt = crypto.createHash('md5').update(password).digest('hex');
+
+    return encrypt;
+  }
+}
+
+module.exports = { HashPassMethods };
