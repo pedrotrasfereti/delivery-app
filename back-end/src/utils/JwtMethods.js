b1d8a0b51cbbb17bc12615f1bd93b5f9919c3dff
Emanoel Mattos <emanoelmattos13@gmail.com>
2022-05-02 17:39:31 -0300

    refactor: rebase again

---
+++ back-end/src/utils/JwtMethods.js
@@ -0,0 +1,27 @@
+import jwt from 'jsonwebtoken';
+import * as fs from 'fs';
+
+class JwtMethods {
+  static jwtSign(payload) {
+    const sign = jwt.sign(payload, JWT_SECRET, {
+      expiresIn: '5h',
+      algorithm: 'HS256',
+    });
+
+    return sign;
+  }
+
+  static verifyToken(token) {
+    const verifying = jwt.verify(token, JWT_SECRET, { algorithms: ['HS256'] });
+
+    return verifying;
+  }
+
+  static decodeToken(token) {
+    const decode = jwt.decode(token);
+
+    return decode;
+  }
+};
+
+export { JwtMethods };
