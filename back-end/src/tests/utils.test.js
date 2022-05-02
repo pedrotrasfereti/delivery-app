ae2df30a8f7817315e9c95edacc6a791eca06ec4
Emanoel Mattos <emanoelmattos13@gmail.com>
2022-05-02 18:52:37 -0300

    feat: implements jwt simple tests

--- back-end/src/tests/utils.test.js
+++ back-end/src/tests/utils.test.js
@@ -1,17 +1,26 @@
 const sinon = require("sinon");
 const chai = require("chai");
-const JwtMethods = require('..')
+const { JwtMethods } = require('../utils/JwtMethods');
+const { jwtPayload, fakeToken } = require("./mocks/Utils");
 
 const { expect } = chai;
 
-describe('aaa', ()=> {
-  it('bb', () => {
-    const signToken = JwtMethods.jwtSign({
-      email: 'Tiradentes@maieiou.com',
-      role: 'admin',
+describe('Testa as funções úteis do backend', () => {
+  describe('Testa a JwtMethods', () => {
+    it('Testa se a sign method retorna um token', () => {
+      const signToken = JwtMethods.jwtSign(jwtPayload);
+  
+      expect(signToken).to.be.a('string');
     });
-    console.log(signToken);
+    it('Testa se a verificação do token retorna um objeto', () => {
+      const verify = JwtMethods.verifyToken(fakeToken);
 
-    expect(signToken).to.be.a('string');
-  }); 
+      expect(verify).to.be.an('object');
+    });
+    it('Testa o método decode', () => {
+      const verify = JwtMethods.decodeToken(fakeToken);
+
+      expect(verify).to.be.an('object');
+    });
+  });
 });
