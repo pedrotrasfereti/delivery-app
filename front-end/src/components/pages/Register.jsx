68d4e21543700b3b45324401836d3b3454949cd5
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-05-03 18:09:28 -0300

    feat: implement register page feats

--- front-end/src/components/pages/Register.jsx
+++ front-end/src/components/pages/Register.jsx
@@ -1,7 +1,20 @@
 import * as React from 'react';
 
+/* Children */
+import DuoGeneric from '../templates';
+
 function Register() {
-  return <h1>Register</h1>;
+  return (
+    <DuoGeneric id="register-page">
+      <div id="form-content">
+        <h1 id="title">Sign Up to Deliveree</h1>
+
+        {/* Here goes the sign up form */}
+      </div>
+
+      {/* <Teaser id="teaser" /> */}
+    </DuoGeneric>
+  );
 }
 
 export default Register;
