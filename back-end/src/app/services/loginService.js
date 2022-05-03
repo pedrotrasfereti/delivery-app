1c04f0b6c2d634df77a1d72562627d37950c51b5
Emanoel Mattos <emanoelmattos13@gmail.com>
2022-05-03 17:02:46 -0300

    feat: implements new login service method

--- back-end/src/app/services/loginService.js
+++ back-end/src/app/services/loginService.js
@@ -1,11 +1,36 @@
-const loginModel = require('../models/loginModel');
-const { jwtSign } = require('../utils/JwtMethods');
+const { JwtMethods } = require('../utils/JwtMethods');
+const { HashPassMethods } = require('../utils/HashPassMethods');
+const userModel = require('../models/userModel');
+
 
 module.exports = {
   async login(data) {
-    const email = await loginModel.login(data);
-    const token = jwtSign({ email });
-    return token;
+    const user = await userModel.getOne(data);
+
+    if(!user) {
+      const err = new Error('Invalid email or password');
+      err.name = 'unauthorized';
+      throw err;
+    }
+
+    const { name, email, role, password } = user;
+    const cryptoPassword = HashPassMethods.encryptPass(data.password);
+
+    const compare = HashPassMethods.comparePass(cryptoPassword, password);
+
+    if(!compare) {
+      const err = new Error('Invalid email or password');
+      err.name = 'unauthorized';
+      throw err;
+    }
+
+    const token = JwtMethods.jwtSign({ email, role });
+    return {
+      name,
+      email,
+      role,
+      token,
+    };
   },
 
 };
