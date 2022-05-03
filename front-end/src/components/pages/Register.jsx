8da3396984bcd3267d4081a9532d943b831c8ef9
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-05-03 18:57:00 -0300

    feat: add basic styles to Register page

--- front-end/src/components/pages/Register.jsx
+++ front-end/src/components/pages/Register.jsx
@@ -7,14 +7,46 @@ import { Packages } from '../../assets';
 import Teaser from '../atoms/Teaser';
 import DuoGeneric from '../templates';
 
+/* Styles */
+import { styled } from '../../stitches.config';
+
+const ContentWrapper = styled('div', {
+  boxShadow: '0 0 120px 10px hsla(215, 0%, 20%, .5)',
+  display: 'flex',
+  flexFlow: 'column nowrap',
+  gap: '$4',
+  padding: '$6 $7',
+  position: 'relative',
+  zIndex: '$1',
+
+  '&>h1': {
+    color: '$textDark',
+    fontFamily: '$sans2',
+    fontSize: '$7',
+    fontWeight: '$7',
+  },
+
+  // Breakpoints
+  '@bp4': {
+    padding: '$5 $4',
+  },
+  '@bp3': {
+    padding: '$4',
+
+    '&>h1': {
+      fontSize: '$6',
+    },
+  },
+});
+
 function Register() {
   return (
     <DuoGeneric id="register-page">
-      <div id="form-content">
+      <ContentWrapper id="form-content">
         <h1 id="title">Sign Up to Deliveree</h1>
 
         {/* Here goes the sign up form */}
-      </div>
+      </ContentWrapper>
 
       <Teaser id="teaser" image={ Packages } />
     </DuoGeneric>
