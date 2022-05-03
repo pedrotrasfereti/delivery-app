eed667c994f6e3f7017267188c3b90493131ef9f
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-05-03 18:49:27 -0300

    feat: create '/register' route

--- front-end/src/routes/index.js
+++ front-end/src/routes/index.js
@@ -2,12 +2,13 @@ import * as React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 /* Children */
-import { Base, Login } from '../components/pages';
+import { Base, Login, Register } from '../components/pages';
 
 function AppRoutes() {
   return (
     <Router>
       <Routes>
+        <Route path="/register" element={ <Register /> } />
         <Route path="/login" element={ <Login /> } />
         <Route path="/" element={ <Base /> } />
       </Routes>
