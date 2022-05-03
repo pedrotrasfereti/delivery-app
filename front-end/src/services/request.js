3b13e3426237882f2dcf24e3da59ecc23b3dd7da
Emanoel Mattos <emanoelmattos13@gmail.com>
2022-05-03 19:38:07 -0300

    style: fix lint troubles

--- front-end/src/services/request.js
+++ front-end/src/services/request.js
@@ -1,17 +1,17 @@
-//https://axios-http.com/ptbr/docs/instance
+// https://axios-http.com/ptbr/docs/instance
 const axios = require('axios');
 
 const instanceApi = axios.create({
   baseURL: `http://localhost:${process.env.REACT_APP_BACKEND_PORT || '3001'}`,
 });
 
-const loginRequest = (endpoint, body) => {
+const loginRequest = async (endpoint, body) => {
   const { data } = await instanceApi.post(endpoint, body);
 
   return data;
 };
 
-export { 
+export {
   instanceApi,
   loginRequest,
 };
