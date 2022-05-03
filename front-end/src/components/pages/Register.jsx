18fb48ae312f2bc2f8c18183933294b6226a269f
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-05-03 17:55:13 -0300

    feat: create Register page component

---
+++ front-end/src/components/pages/Register.jsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+function Register() {
+  return <h1>Register</h1>;
+}
+
+export default Register;
--- front-end/src/components/pages/index.js
+++ front-end/src/components/pages/index.js
@@ -1,2 +1,3 @@
 export { default as Base } from './Base';
 export { default as Login } from './Login';
+export { default as Register } from './Register';
