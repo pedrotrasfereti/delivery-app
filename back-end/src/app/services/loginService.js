b57bdaac1b652d3b205ada53d2013135f114d092
Joao Vitor <jvitor.spaula@gmail.com>
2022-05-03 18:57:38 -0300

    fix: fix lint in loginService

--- back-end/src/app/services/loginService.js
+++ back-end/src/app/services/loginService.js
@@ -2,35 +2,25 @@ const { JwtMethods } = require('../utils/JwtMethods');
 const { HashPassMethods } = require('../utils/HashPassMethods');
 const userModel = require('../models/userModel');
 
-
 module.exports = {
   async login(data) {
-    const user = await userModel.getOne(data);
+    try {
+      const user = await userModel.getOne(data);
 
-    if(!user) {
+      if (!user) throw Error;
+
+      const { name, email, role, password } = user;
+      const cryptoPassword = HashPassMethods.encryptPass(data.password);
+      const compare = HashPassMethods.comparePass(cryptoPassword, password);
+
+      if (!compare) throw Error;
+      const token = JwtMethods.jwtSign({ email, role });
+      return { name, email, role, token };
+    } catch (error) {
       const err = new Error('Invalid email or password');
       err.name = 'unauthorized';
       throw err;
     }
-
-    const { name, email, role, password } = user;
-    const cryptoPassword = HashPassMethods.encryptPass(data.password);
-
-    const compare = HashPassMethods.comparePass(cryptoPassword, password);
-
-    if(!compare) {
-      const err = new Error('Invalid email or password');
-      err.name = 'unauthorized';
-      throw err;
-    }
-
-    const token = JwtMethods.jwtSign({ email, role });
-    return {
-      name,
-      email,
-      role,
-      token,
-    };
   },
 
 };
