8bbbd0e3170656f09e5965901faa5f6304ffd412
enioandrade <enio.goulart@gmail.com>
2022-05-02 23:18:48 -0300

    feat: created error middleware

---
+++ back-end/src/api/middlewares/error-handler.js
@@ -0,0 +1,15 @@
+module.exports = (err, _req, res, _next) => {
+  const { name, message } = err;
+  switch (name) {
+    case 'unauthorized': 
+      res.status(401).json({ message });
+      break;
+    case 'bad_request':
+      res.status(400).json({ message });
+      break;
+  
+    default:
+      res.status(500).json({ message });
+      break;
+  }
+};
