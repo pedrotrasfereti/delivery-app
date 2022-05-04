e9dbc041e853a9ba9c92aee7123bfbfaaa101463
Joao Vitor <jvitor.spaula@gmail.com>
2022-05-04 18:54:20 -0300

    feat: add customer/checkout router

--- back-end/src/api/api.js
+++ back-end/src/api/api.js
@@ -3,6 +3,7 @@ const loginRouter = require('./routers/loginRouter');
 const userRouter = require('./routers/userRouter');
 const productRouter = require('./routers/productRoutes');
 const errorHandler = require('./middlewares/error-handler');
+const salesRouter = require('./routers/salesRouter');
 
 const api = express();
 
@@ -11,6 +12,7 @@ api.use(express.json());
 api.use('/register', userRouter);
 api.use('/login', loginRouter);
 api.use('/customer/product', productRouter);
+api.use('/customer/checkout', salesRouter)
 
 api.use(errorHandler);
 
