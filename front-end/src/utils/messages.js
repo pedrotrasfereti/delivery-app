a67a15c90df723bb8aeb9a546b3b8f0019d024f8
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-05-04 14:41:02 -0300

    feat: add new error messages to utils

--- front-end/src/utils/messages.js
+++ front-end/src/utils/messages.js
@@ -1,6 +1,12 @@
 const messages = {
+  name: {
+    invalid: 'Name must be at least 12 characters long',
+  },
   email: {
     invalid: 'This email is invalid. Make sure it\'s written like example@email.com',
+  },
+  password: {
+    invalid: 'Password must be at least 6 characters long',
   },
 };
 
