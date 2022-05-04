341ea55052e9474d7cb40e06b4a208a513882a28
Joao Vitor <jvitor.spaula@gmail.com>
2022-05-04 19:01:23 -0300

    feat: implements createSale in salesService

---
+++ back-end/src/app/services/salesService.js
@@ -0,0 +1,8 @@
+const salesModel = require('../models/salesModel');
+
+module.exports = {
+    async createSale(obj) {
+        const created = await salesModel.createSale(obj);
+        return created;
+    }
+}
