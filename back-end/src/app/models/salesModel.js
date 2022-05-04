4eaf93de993a070db925fa809bcaa9be4f59c5d0
Joao Vitor <jvitor.spaula@gmail.com>
2022-05-04 19:22:04 -0300

    refactor: refactor the createSale function

--- back-end/src/app/models/salesModel.js
+++ back-end/src/app/models/salesModel.js
@@ -2,7 +2,15 @@ const { Sales } = require('../../database/models');
 
 module.exports = {
     async createSale(obj) {
-        const sale = await Sales.create({...obj});
+        const sale = await Sales.create({
+            userId: obj.userId,
+            sellerId: obj.sellerId,
+            totalPrice: obj.totalPrice,
+            deliveryAddress: obj.deliveryAddress,
+            deliveryNumber: obj.deliveryNumber,
+            saleDate: new Date(),
+            status: obj.status
+        });
         return sale;
     },
 }
