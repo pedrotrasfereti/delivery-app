05a0dc6fe93bddff35ef6461c5268719b52ebc2b
enioandrade <enio.goulart@gmail.com>
2022-05-04 15:50:58 -0300

    feat: created product msc, methods: getAll

---
+++ back-end/src/app/controllers/productController.js
@@ -0,0 +1,9 @@
+const productService = require('../services/productService');
+
+module.exports = {
+  async getAll(_req, res) {
+    const products = await productService.getAll({ raw: true });
+    
+    return res.status(200).json(products);
+  },
+};
