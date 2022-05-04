2a7f32a7e5cff989d0989d6e9eb8015cc7ce3f42
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-05-04 18:11:21 -0300

    feat: add cors middleware and comments to api.js

--- back-end/src/api/api.js
+++ back-end/src/api/api.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const loginRouter = require('./routers/loginRouter');
 const userRouter = require('./routers/userRouter');
 const productRouter = require('./routers/productRoutes');
@@ -6,12 +7,16 @@ const errorHandler = require('./middlewares/error-handler');
 
 const api = express();
 
+// middlewares
 api.use(express.json());
+api.use(cors());
 
+// routes
 api.use('/register', userRouter);
 api.use('/login', loginRouter);
 api.use('/customer/product', productRouter);
 
+// error middlewares
 api.use(errorHandler);
 
 module.exports = api;
