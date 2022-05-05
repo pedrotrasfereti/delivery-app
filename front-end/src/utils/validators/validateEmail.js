c9bd7f00084883cecd3ead5e60f585645cd28017
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-05-05 11:24:59 -0300

    feat: update email validator

--- front-end/src/utils/validators/validateEmail.js
+++ front-end/src/utils/validators/validateEmail.js
@@ -1,7 +1,7 @@
 const EMAIL_REGEX = /^[a-z0-9_.-]+@[a-z]+\.[a-z]{2,3}(?:\.[a-z]{2})?$/;
 
 const validateEmail = (email, callback) => {
-  if (email && !EMAIL_REGEX.test(email)) {
+  if (!EMAIL_REGEX.test(email)) {
     callback(true);
   } else {
     callback(false);
