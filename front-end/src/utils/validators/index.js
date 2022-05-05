8d51b3e4a1e958cbf2e064c3ba492d9cff26bb8d
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-05-05 10:33:08 -0300

    refactor: move 'validate...' functions to utils/validators

---
+++ front-end/src/utils/validators/index.js
@@ -0,0 +1,4 @@
+export { default as validateEmail } from './validateEmail';
+export { default as validateMatch } from './validateMatch';
+export { default as validateName } from './validateName';
+export { default as validatePassword } from './validatePassword';
