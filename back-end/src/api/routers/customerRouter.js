5e7328dcccb0bcd690cdd73a4ce4e43ca6bf7b6a
enioandrade <enio.goulart@gmail.com>
2022-05-05 19:45:02 -0300

    refactor: add router '/orders/:id' & '/orders'

--- back-end/src/api/routers/customerRouter.js
+++ back-end/src/api/routers/customerRouter.js
@@ -6,6 +6,8 @@ const { validateUserToken } = require('../middlewares/validate-user-token');
 
 const customerRouter = Router();
 customerRouter.post('/checkout', validateUserToken, rescue(salesController.createSale));
+customerRouter.get('/orders/:id', validateUserToken, rescue(salesController.getSale));
+customerRouter.get('/orders', validateUserToken, rescue(salesController.getSales));
 customerRouter.get('/product', validateUserToken, rescue(productController.getAll));
 
 module.exports = customerRouter;
