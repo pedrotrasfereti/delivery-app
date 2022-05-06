b89233e79d3f3b0c649d5aad2de6deb0111fb5d1
Emanoel Mattos <emanoelmattos13@gmail.com>
2022-05-06 14:36:33 -0300

    feat: add new error case not found

--- back-end/src/api/middlewares/error-handler.js
+++ back-end/src/api/middlewares/error-handler.js
@@ -7,6 +7,9 @@ module.exports = (err, _req, res, _next) => {
     case 'bad_request':
       res.status(400).json({ message });
       break;
+    case 'not_found':
+      res.status(404).json({ message });
+      break;
     default:
       res.status(500).json({ message });
       break;
