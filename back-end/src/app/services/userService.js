1784d9e8cd60be7536b4b5850eec98820ca7f051
Emanoel Mattos <emanoelmattos13@gmail.com>
2022-05-06 15:46:25 -0300

    feat: implements conflict error validation on userService

--- back-end/src/app/services/userService.js
+++ back-end/src/app/services/userService.js
@@ -3,8 +3,15 @@ const { JwtMethods } = require('../utils/JwtMethods');
 
 module.exports = {
   async create(user) {
-    const { name, email, role } = await userModel.create(user);
-    const token = JwtMethods.jwtSign({ email, role });
-    return { name, email, role, token };
+    try {
+      const createNewUser = await userModel.create(user);
+      const { name, email, role } = createNewUser;
+      const token = JwtMethods.jwtSign({ email, role });
+      return { name, email, role, token };
+    } catch (error) {
+      const err = new Error('User already exist');
+      err.name = 'conflict';
+      throw err;
+    }
   },
 };
