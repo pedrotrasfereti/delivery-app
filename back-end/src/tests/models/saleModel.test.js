c6a2813e7fe278c316888a3753ef1d3050031a16
Matheus <matheus.pormanhani@gmail.com>
2022-05-06 16:38:44 -0300

    test: refactor models tests

--- back-end/src/tests/models/saleModel.test.js
+++ back-end/src/tests/models/saleModel.test.js
@@ -1,39 +1,86 @@
 const { expect } = require('chai');
 const sinon = require('sinon');
-const { Sales } = require('../../database/models');
-const salesModelApp = require('../../app/models/salesModel');
+const { Sales, Products } = require('../../database/models');
+const salesModel = require('../../app/models/salesModel');
+const salesproductsModel = require('../../app/models/salesproductsModel');
 
 describe("Testing sales model", () => {
-  describe("Testing createSale method", () => {
-    const mockSale = {
-      id: 1,
-      userId: 3,
-      sellerId: 2,
-      totalPrice: 20.35,
-      deliveryAddress: "antonio marcos da cruz",
-      deliveryNumber: "571",
-      saleDate: "2020-05-02T00:00:00.000Z",
-      status: "in progress"
+  const mockSale = {
+    id: 1,
+    userId: 3,
+    sellerId: 2,
+    totalPrice: 20.35,
+    deliveryAddress: "antonio marcos da cruz",
+    deliveryNumber: "571",
+    saleDate: "2020-05-02T00:00:00.000Z",
+    status: "in progress"
+  }
+  const mockSaleApp = {
+    userId: 3,
+    sellerId: 2,
+    totalPrice: 20.35,
+    deliveryAddress: "antonio marcos da cruz",
+    deliveryNumber: "571",
+    status: "in progress"
+  }
+  const mockProducts = [
+    {
+      productId: 1,
+      quantity: 2
     }
-    const mockSaleApp = {
-      userId: 3,
-      sellerId: 2,
-      totalPrice: 20.35,
-      deliveryAddress: "antonio marcos da cruz",
-      deliveryNumber: "571",
-      status: "in progress"
-    }
+  ]
 
-    afterEach(() => {
-      sinon.restore();
-    })
+  afterEach(() => {
+    sinon.restore();
+  })
+
+  describe("Testing createSale method", () => {
     it("Return a created sale", async () => {
       const createStub = sinon.stub(Sales, "create").resolves(mockSale)
-      const saleCreated = await salesModelApp.createSale(mockSaleApp)
+      const createProductsStub = sinon.stub(salesproductsModel, "create").resolves()
+      sinon.stub(Date, "now").returns(mockSale.saleDate)
+      const saleCreated = await salesModel.createSale(mockSaleApp, mockProducts)
 
-      sinon.assert.calledWith(createStub, { ...mockSaleApp, saleDate: new Date() });
+      sinon.assert.calledWith(createStub, { ...mockSaleApp, saleDate: mockSale.saleDate })
+      sinon.assert.calledWith(createProductsStub, [{ saleId: mockSale.id, ...mockProducts[0] }]);
       expect(saleCreated).to.be.a("object");
       expect(saleCreated).to.be.deep.equal(mockSale);
+    });
+  })
+
+  describe("Testing getSales method", () => {
+    const id = 1
+
+    it("Return an array of sales", async () => {
+      const findAllStub = sinon.stub(Sales, "findAll").resolves([mockSale])
+      const sales = await salesModel.getSales(id)
+
+      sinon.assert.calledWith(findAllStub, { where: { userId: id }, raw: true });
+      expect(sales).to.be.a("array");
+      expect(sales).to.be.deep.equal([mockSale]);
+    })
+  })
+
+  describe("Testing getSale method", () => {
+    const id = 1
+    const findOneQuery = {
+      where: { id },
+      include: [{
+        model: Products,
+        as: 'products',
+        through: {
+          attributes: ['quantity'],
+        },
+      }],
+    }
+
+    it("Return one sale", async () => {
+      const findOneStub = sinon.stub(Sales, "findOne").resolves(mockSale)
+      const sales = await salesModel.getSale(id)
+
+      sinon.assert.calledWith(findOneStub, findOneQuery);
+      expect(sales).to.be.a("object");
+      expect(sales).to.be.deep.equal(mockSale);
     })
   })
 })
--- back-end/src/tests/models/userModel.test.js
+++ back-end/src/tests/models/userModel.test.js
@@ -1,60 +1,51 @@
-const {
-  expect
-} = require('chai');
-const chai = require('chai');
+const { expect } = require('chai');
 const sinon = require('sinon');
-const {
-  Users
-} = require('../../database/models/');
+const { Users } = require('../../database/models/');
 
-const userModelApp = require('../../app/models/userModel');
+const userModel = require('../../app/models/userModel');
+const { HashPassMethods } = require('../../app/utils/HashPassMethods');
 
 describe('Testing user model', () => {
+  const mockDatabase = {
+    id: "5",
+    name: "joao",
+    email: "jvitor@gmail.com",
+    password: "e10adc3949ba59abbe56e057f20f883e",
+    role: "customer"
+  }
+  const mockUser = {
+    name: "joao",
+    email: "jvitor@gmail.com",
+    password: "123456",
+  }
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
   describe('Testing create method', () => {
-    const mockDatabase = {
-      id: "5",
-      name: "joao",
-      email: "jvitor@gmail.com",
-      password: "e10adc3949ba59abbe56e057f20f883e",
-      role: "customer"
-    }
-    const mockApp = {
-      name: "joao",
-      email: "jvitor@gmail.com",
-      password: "123456",
-    }
-    before(() => {
-      sinon.stub(Users, "create").resolves(mockDatabase);
-    });
-    after(() => {
-      sinon.restore();
-    });
     it("Return the object user created", async () => {
-      const user = await userModelApp.create(mockApp);
+      const encryptPassStub = sinon.stub(HashPassMethods, 'encryptPass').returns('encriptedPassword');
+      const createUserStub = sinon.stub(Users, "create").resolves(mockDatabase);
+
+      const user = await userModel.create(mockUser);
+
+      sinon.assert.calledWith(encryptPassStub, mockUser.password);
+      sinon.assert.calledWith(createUserStub, {
+        ...mockUser,
+        role: 'customer',
+        password: 'encriptedPassword',
+      })
       expect(user).to.be.a("object");
       expect(user).to.be.equal(mockDatabase);
     })
   })
 
   describe("Testing getOne method", () => {
-    const mockDatabase = {
-      id: "5",
-      name: "joao",
-      email: "jvitor.spaula@gmail.com",
-      password: "e10adc3949ba59abbe56e057f20f883e",
-      role: "customer"
-    }
-    const mockApp = {
-      email: "jvitor.spaula@gmail.com",
-      password: "123456"
-    }
-    afterEach(() => {
-      sinon.restore();
-    });
     describe('If user exists', () => {
       it("getOne method return user", async () => {
         const findOneStub = sinon.stub(Users, "findOne").resolves(mockDatabase);
-        const user = await userModelApp.getOne(mockApp);
+        const user = await userModel.getOne(mockUser);
         sinon.assert.calledWith(findOneStub, {
           where: {
             email: mockDatabase.email,
@@ -72,8 +63,8 @@ describe('Testing user model', () => {
     describe('If user does not exists', () => {
       it("getOne method return null", async () => {
         const findOneStub = sinon.stub(Users, "findOne").resolves(null);
-        const user = await userModelApp.getOne(mockApp);
-        sinon.assert.calledWith(findOneStub, { where: { email: mockApp.email }, raw: true });
+        const user = await userModel.getOne(mockUser);
+        sinon.assert.calledWith(findOneStub, { where: { email: mockUser.email }, raw: true });
         expect(user).to.be.equal(null);
       })
     })
