442026595f5911c7f07f60e3165b87fd55532bab
Matheus <matheus.pormanhani@gmail.com>
2022-05-06 16:43:17 -0300

    refactor: move validate-user-token to api.js

--- back-end/src/api/routers/customerRouter.js
+++ back-end/src/api/routers/customerRouter.js
@@ -2,12 +2,11 @@ const { Router } = require('express');
 const rescue = require('express-rescue');
 const salesController = require('../../app/controllers/salesController');
 const productController = require('../../app/controllers/productController');
-const { validateUserToken } = require('../middlewares/validate-user-token');
 
 const customerRouter = Router();
-customerRouter.post('/checkout', validateUserToken, rescue(salesController.createSale));
-customerRouter.get('/orders/:id', validateUserToken, rescue(salesController.getSale));
-customerRouter.get('/orders', validateUserToken, rescue(salesController.getSales));
-customerRouter.get('/product', validateUserToken, rescue(productController.getAll));
+customerRouter.post('/checkout', rescue(salesController.createSale));
+customerRouter.get('/orders/:id', rescue(salesController.getSale));
+customerRouter.get('/orders', rescue(salesController.getSales));
+customerRouter.get('/product', rescue(productController.getAll));
 
 module.exports = customerRouter;
