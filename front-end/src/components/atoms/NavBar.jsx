c019876e3d306770ad2ff37191c6eedbefa375cd
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-05-07 17:18:33 -0300

    feat: render 'children' prop inside NavBar component

--- front-end/src/components/atoms/NavBar.jsx
+++ front-end/src/components/atoms/NavBar.jsx
@@ -1,7 +1,23 @@
 import * as React from 'react';
+import PropTypes from 'prop-types';
 
-function NavBar() {
-  return <nav />;
+function NavBar({ children }) {
+  return (
+    <nav>
+      { children }
+    </nav>
+  );
 }
+
+NavBar.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]),
+};
+
+NavBar.defaultProps = {
+  children: '',
+};
 
 export default NavBar;
--- front-end/src/components/molecules/Header.jsx
+++ front-end/src/components/molecules/Header.jsx
@@ -1,7 +1,22 @@
 import * as React from 'react';
 
-function Header() {
-  return <header />;
+function Header({ children }) {
+  return (
+    <header>
+      { children }
+    </header>
+  );
 }
+
+Header.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]),
+};
+
+Header.defaultProps = {
+  children: '',
+};
 
 export default Header;
