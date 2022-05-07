8ff2b325da658e9980b1a5a61b31e04f8ade5850
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-05-07 16:35:26 -0300

    feat: create Header component

---
+++ front-end/src/components/molecules/Header.jsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+function Header() {
+  return <header />;
+}
+
+export default Header;
--- front-end/src/components/molecules/index.js
+++ front-end/src/components/molecules/index.js
@@ -1,2 +1,3 @@
 export { default as ErrorMessageBox } from './ErrorMessageBox';
+export { default as Header } from './Header';
 export { default as TextInputLabel } from './TextInputLabel';
