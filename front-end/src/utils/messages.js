9aa4502946362938c179a8fcb39a7283fc095e0a
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-05-07 10:44:37 -0300

    feat: add punctuation to error messages

--- front-end/src/utils/messages.js
+++ front-end/src/utils/messages.js
@@ -1,19 +1,19 @@
 const messages = {
   name: {
-    invalid: 'Name must be at least 12 characters long',
+    invalid: 'Name must be at least 12 characters long.',
   },
   email: {
     invalid: 'This email is invalid. Make sure it\'s written like example@email.com',
   },
   password: {
-    invalid: 'Password must be at least 6 characters long',
-    mismatch: 'Passwords did not match',
+    invalid: 'Password must be at least 6 characters long.',
+    mismatch: 'Passwords did not match.',
   },
   login: {
-    notFound: 'Incorrect email or password',
+    notFound: 'Incorrect email or password.',
   },
   register: {
-    conflict: 'Name or email is already associated to an account',
+    conflict: 'Name or email is already associated to an account.',
   },
 };
 
