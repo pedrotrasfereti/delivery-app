7910c48fadeb412ba07fb0df992df8d2e3cb9f42
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-05-07 15:37:50 -0300

    feat: create '/customer/products' route

--- front-end/src/routes/index.js
+++ front-end/src/routes/index.js
@@ -2,12 +2,18 @@ import * as React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 /* Children */
-import { Base, Login, Register } from '../components/pages';
+import {
+  Base,
+  Login,
+  Register,
+  Products,
+} from '../components/pages';
 
 function AppRoutes() {
   return (
     <Router>
       <Routes>
+        <Route path="/customer/products" element={ <Products /> } />
         <Route path="/register" element={ <Register /> } />
         <Route path="/login" element={ <Login /> } />
         <Route path="/" element={ <Base /> } />
