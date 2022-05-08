b6f6c28ab5946df363b0c12399ebb26677292a74
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-05-07 21:05:46 -0300

    feat: create 'navLinksMap' util

---
+++ front-end/src/utils/navLinksMap.js
@@ -0,0 +1,26 @@
+const navLinksMap = (username) => ({
+  '/customer/products': [
+    {
+      dataTestId: 'customer_products__element-navbar-link-products',
+      name: 'Products',
+      to: '/customer/products',
+    },
+    {
+      dataTestId: 'customer_products__element-navbar-link-orders',
+      name: 'My orders',
+      to: '/',
+    },
+    {
+      dataTestId: 'customer_products__element-navbar-user-full-name',
+      name: username,
+      to: '/',
+    },
+    {
+      dataTestId: 'customer_products__element-navbar-link-logout',
+      name: 'Logout',
+      to: '/login',
+    },
+  ],
+});
+
+export default navLinksMap;
