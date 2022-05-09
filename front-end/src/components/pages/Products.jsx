f6c98a39d5636e5e681d7bb3aaa7096f3224409c
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-05-09 09:29:24 -0300

    feat: update Products page

--- front-end/src/components/pages/Products.jsx
+++ front-end/src/components/pages/Products.jsx
@@ -1,38 +1,49 @@
 import * as React from 'react';
 
+/* Children */
+import { Sidebar } from '../atoms';
+import { Header } from '../molecules';
+import { ProductCards } from '../organisms';
+
+/* Styled */
+import { styled } from '../../stitches.config';
+
+const StitchesComponent = styled('div', {
+  minHeight: '100vh',
+
+  '&>.Container': {
+    display: 'flex',
+    paddingTop: '5rem',
+    minHeight: '100vh',
+  },
+});
+
+const Main = styled('div', {
+  backgroundColor: '$gray100',
+  position: 'relative',
+  width: '100%',
+  zIndex: '0',
+});
+
 function Products() {
   return (
-    <div id="products-page">
+    <StitchesComponent id="products-page">
       {/* Header */}
-      <header>
-        <nav id="header-nav" />
-      </header>
+      <Header />
 
-      {/* Sidebar */}
-      <aside>
-        <section />
-      </aside>
+      <div className="Container">
+        <Sidebar />
 
-      {/* Main */}
-      <main>
-        <section>
-          <article>
-            <h2>1</h2>
-            <p>Lorem ipsum dolor sit amet</p>
-          </article>
-
-          <article>
-            <h2>2</h2>
-            <p>Lorem ipsum dolor sit amet</p>
-          </article>
-        </section>
-      </main>
+        <Main>
+          <ProductCards />
+        </Main>
+      </div>
 
       {/* Footer */}
       <footer>
         <nav id="footer-nav" />
       </footer>
-    </div>
+    </StitchesComponent>
   );
 }
 
