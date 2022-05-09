a23e8b4af7bd0e0df74bee20864d5e839359e4fc
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-05-09 09:17:23 -0300

    feat: style NavBar component

--- front-end/src/components/atoms/NavBar.jsx
+++ front-end/src/components/atoms/NavBar.jsx
@@ -1,11 +1,57 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
 
+/* Styles */
+import { styled } from '../../stitches.config';
+
+const StitchesComponent = styled('nav', {
+  '& ul': {
+    display: 'flex',
+    gap: '$5',
+
+    '& li': {
+      alignItems: 'center',
+      display: 'flex',
+      gap: '$2',
+    },
+
+    '& li>a': {
+      color: '$textDark',
+      fontFamily: '$sans',
+      fontSize: '$3',
+      fontWeight: '$4',
+      position: 'relative',
+    },
+
+    '& li>a:hover': {
+      color: '$primary',
+    },
+
+    '& li>a::before': {
+      backgroundColor: '$secondary',
+      border: '0',
+      borderRadius: '$default',
+      content: '',
+      height: '.1rem',
+      position: 'absolute',
+      top: '1.4rem',
+      left: '0',
+      width: '100%',
+      transform: 'scaleX(0)',
+      transition: 'transform .3s ease',
+    },
+
+    '& li>a:hover::before': {
+      transform: 'scaleX(1)',
+    },
+  },
+});
+
 function NavBar({ children }) {
   return (
-    <nav>
+    <StitchesComponent>
       { children }
-    </nav>
+    </StitchesComponent>
   );
 }
 
