a817e577c184a4819898daedc71e4abd9ed9bd31
Matheus <matheus.pormanhani@gmail.com>
2022-05-11 12:11:02 -0300

    chore: remove unused seller controller, service and model

--- back-end/src/app/services/sellerService.js
+++
@@ -1,8 +0,0 @@
-const sellerModel = require('../models/sellerModel');
-
-module.exports = {
-    async getAllSalesService(id) {
-        const getSales = await sellerModel.getAllSalesModel(id);
-        return getSales;
-    },
-};
