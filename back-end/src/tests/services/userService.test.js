3abbc4fcb586282ad868b930b7add35fb3a4553a
Matheus <matheus.pormanhani@gmail.com>
2022-05-11 12:58:55 -0300

    test: add tests in userService.test.js

--- back-end/src/tests/services/userService.test.js
+++ back-end/src/tests/services/userService.test.js
@@ -11,19 +11,34 @@ describe('Tests userService', () => {
       sinon.restore();
     })
 
-    it('Returns created user', async () => {
-      const { id, role, email } = userMock;
-      const createStub = sinon.stub(userModel, 'create').resolves(userMock);
-      const jwtSignStub = sinon.stub(JwtMethods, 'jwtSign').returns('token');
-      const user = await userService.create(userMock);
+    describe('When user already exist', () => {
+      it('Should throw error message "User already Exist', async () => {
+        const errorStub = sinon.stub(userModel, "create").rejects(new Error());
+        try {
+          await userService.create(userMock)
+          sinon.assert.calledWith(errorStub, userMock);
+        } catch (error) {
+          expect(error.message).to.equal("User already exist");
+          expect(error.name).to.equal("conflict");
+        }
+      })
+    })
 
-      sinon.assert.calledWith(createStub, userMock);
-      sinon.assert.calledWith(jwtSignStub, { id, email, role });
-      expect(user).to.be.a('object');
-      expect(user).to.be.deep.equal({
-        ...userMock,
-        token: 'token',
-      });
+    describe('When user does not exist', () => {
+      it('Returns created user', async () => {
+        const { id, role, email } = userMock;
+        const createStub = sinon.stub(userModel, 'create').resolves(userMock);
+        const jwtSignStub = sinon.stub(JwtMethods, 'jwtSign').returns('token');
+        const user = await userService.create(userMock);
+
+        sinon.assert.calledWith(createStub, userMock);
+        sinon.assert.calledWith(jwtSignStub, { id, email, role });
+        expect(user).to.be.a('object');
+        expect(user).to.be.deep.equal({
+          ...userMock,
+          token: 'token',
+        });
+      })
     })
   })
 })
