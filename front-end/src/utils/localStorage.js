e3c334aa5f9585ed7958b46d1b7e259f45c1b94c
Emanoel Mattos <emanoelmattos13@gmail.com>
2022-05-11 14:44:39 -0300

    feat: create localStorage methods

---
+++ front-end/src/utils/localStorage.js
@@ -0,0 +1,15 @@
+export default class LocalStorageMethods {
+  static getItem(item) {
+    const localItem = localStorage.getItem(item);
+    return localItem;
+  }
+
+  static setItem(key, payload) {
+    localStorage.setItem(key, JSON.stringify(payload));
+  }
+
+  static parseItem(payload) {
+    const parse = JSON.parse(payload);
+    return parse;
+  }
+}
