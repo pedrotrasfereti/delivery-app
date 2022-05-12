e616a46a168adb64d4f06d09b040574806a0d732
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-05-12 18:02:58 -0300

    feat: add checkout products count to CheckoutBtn

--- front-end/src/components/atoms/CheckoutBtn.jsx
+++ front-end/src/components/atoms/CheckoutBtn.jsx
@@ -1,20 +1,37 @@
 import * as React from 'react';
 
+/* State */
+import { useSelector } from 'react-redux';
+
 /* Styles */
 import { styled } from '../../stitches.config';
 
 const StitchesComponent = styled('button', {
-  backgroundColor: '$button',
-  padding: '20px',
+  backgroundColor: '$accent2',
+  bottom: '15px',
   border: '0',
   borderRadius: '$round',
-  position: 'absolute',
-  bottom: '15px',
+  height: '50px',
+  position: 'fixed',
   right: '15px',
+  width: '50px',
+
+  '&>span': {
+    color: '$textLight',
+    fontFamily: '$sans',
+    fontSize: '$4',
+    fontWeight: '$4',
+  },
 });
 
 function CheckoutBtn() {
-  return <StitchesComponent />;
+  const { products } = useSelector((state) => state.checkout);
+
+  return (
+    <StitchesComponent data-testid="customer_products__checkout-bottom-value">
+      <span>{ products.length }</span>
+    </StitchesComponent>
+  );
 }
 
 export default CheckoutBtn;
