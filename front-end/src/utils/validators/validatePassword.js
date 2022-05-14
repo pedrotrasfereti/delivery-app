650603d43dd8e9f566950eb325ea48bd5d75fdc4
Emanoel Mattos <emanoelmattos13@gmail.com>
2022-05-13 21:15:50 -0300

    fix: add underline character to password regex

--- front-end/src/utils/validators/validatePassword.js
+++ front-end/src/utils/validators/validatePassword.js
@@ -1,4 +1,4 @@
-const PASSWORD_REGEX = /^[A-Za-z0-9\s$&+,:;=?@#|'<>.^*()%!-]{6,}$/;
+const PASSWORD_REGEX = /^[A-Za-z0-9\s$&+,:;=?@#|'<>.^*()%!_-]{6,}$/;
 
 const validatePassword = (password) => PASSWORD_REGEX.test(password);
 
