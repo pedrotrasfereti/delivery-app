ed6a042006b2c5e9134c1c1c0fad3026550cd990
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-05-15 19:39:28 -0300

    feat: add icons to navLinksMap util

--- front-end/src/utils/navLinksMap.js
+++ front-end/src/utils/navLinksMap.js
@@ -1,3 +1,13 @@
+import * as React from 'react';
+
+/* Assets */
+import {
+  BsFillBagFill as BagIcon,
+  BsFillBasketFill as BasketIcon,
+  BsDoorClosedFill as DoorClosedIcon,
+  BsPersonFill as PersonIcon,
+} from 'react-icons/bs';
+
 function navLinksMap(username) {
   return ({
     '/customer/products': [
@@ -5,21 +15,25 @@ function navLinksMap(username) {
         dataTestId: 'customer_products__element-navbar-link-products',
         name: 'Products',
         to: '/customer/products',
+        icon: () => <BasketIcon className="Icon" />,
       },
       {
         dataTestId: 'customer_products__element-navbar-link-orders',
         name: 'My orders',
         to: '/',
+        icon: () => <BagIcon className="Icon" />,
       },
       {
         dataTestId: 'customer_products__element-navbar-user-full-name',
         name: username,
         to: '/',
+        icon: () => <PersonIcon className="Icon" />,
       },
       {
         dataTestId: 'customer_products__element-navbar-link-logout',
         name: 'Logout',
         to: '/login',
+        icon: () => <DoorClosedIcon className="Icon" />,
       },
     ],
   });
