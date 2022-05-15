997b5036d4af7235121fd641693fc8473e4ffc55
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-05-15 20:48:57 -0300

    refactor: unhide CheckoutBtn on breakpoint

--- front-end/src/components/pages/Products.jsx
+++ front-end/src/components/pages/Products.jsx
@@ -2,9 +2,6 @@ import React, { useState, useEffect } from 'react';
 
 /* Children */
 import { CheckoutBtn, SideBar } from '../atoms';
-import {
-  StitchesComponent as CheckoutBtnStitches,
-} from '../atoms/CheckoutBtn';
 import { Header } from '../molecules';
 import { ProductCards } from '../organisms';
 
@@ -27,10 +24,6 @@ const StitchesComponent = styled('div', {
   '@bp3': {
     '&>.Container': {
       flexFlow: 'column nowrap',
-    },
-
-    [`&>${CheckoutBtnStitches}, &>${CheckoutBtnStitches}>*`]: {
-      display: 'none',
     },
   },
 });
