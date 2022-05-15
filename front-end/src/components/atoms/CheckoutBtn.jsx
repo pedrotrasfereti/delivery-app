a70b3fd3c5e73010e69cb90a4de11d4dde4f1560
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-05-14 21:00:23 -0300

    feat: checkoutBtn redirects to 'customer/checkout'

--- front-end/src/components/atoms/CheckoutBtn.jsx
+++ front-end/src/components/atoms/CheckoutBtn.jsx
@@ -1,4 +1,5 @@
-import * as React from 'react';
+import React, { useState } from 'react';
+import { Navigate } from 'react-router-dom';
 
 /* State */
 import { useSelector } from 'react-redux';
@@ -7,29 +8,69 @@ import { useSelector } from 'react-redux';
 import { styled } from '../../stitches.config';
 
 const StitchesComponent = styled('button', {
-  backgroundColor: '$accent2',
   bottom: '15px',
   border: '0',
-  borderRadius: '$round',
+  borderRadius: '$edge',
+  display: 'flex',
   height: '50px',
   position: 'fixed',
   right: '15px',
-  width: '50px',
+  width: '150px',
 
-  '&>span': {
-    color: '$textLight',
-    fontFamily: '$sans',
-    fontSize: '$4',
-    fontWeight: '$4',
+  '&:hover:not(:disabled)': {
+    cursor: 'pointer',
+  },
+
+  '&>.customer_products__checkout-label, &>.customer_products__checkout-bottom-value': {
+    alignItems: 'center',
+    display: 'flex',
+    justifyContent: 'center',
+
+    '&>span': {
+      color: '$textLight',
+      fontFamily: '$sans',
+      fontSize: '$4',
+      fontWeight: '$4',
+    },
+  },
+
+  '&>.customer_products__checkout-label': {
+    backgroundColor: '$primary',
+    flex: '1.15',
+  },
+
+  '&>.customer_products__checkout-bottom-value': {
+    backgroundColor: '$accent2',
+    flex: '1',
   },
 });
 
 function CheckoutBtn() {
+  const [shouldRedirect, setShouldRedirect] = useState(false);
+
   const { total } = useSelector((state) => state.checkout);
 
   return (
-    <StitchesComponent data-testid="customer_products__checkout-bottom-value">
-      <span>{ String(total).replace('.', ',') }</span>
+    <StitchesComponent
+      data-testid="customer_products__button-cart"
+      disabled={ !total && true }
+      onClick={ () => setShouldRedirect(true) }
+    >
+      <div className="customer_products__checkout-label">
+        <span>Total:</span>
+      </div>
+      <div className="customer_products__checkout-bottom-value">
+        <span
+          data-testid="customer_products__checkout-bottom-value"
+        >
+          { String(total).replace('.', ',') }
+        </span>
+      </div>
+
+      {/* Redirect */}
+      {
+        shouldRedirect && <Navigate replace to="/customer/checkout" />
+      }
     </StitchesComponent>
   );
 }
