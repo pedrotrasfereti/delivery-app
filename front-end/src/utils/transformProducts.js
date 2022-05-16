d2f0a65948a122a56a53c88a94b2978f6208e4a5
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-05-16 13:05:57 -0300

    fix: delete space before '}' in transformProducts util

--- front-end/src/utils/transformProducts.js
+++ front-end/src/utils/transformProducts.js
@@ -2,7 +2,7 @@ function transformProducts(products) {
   const arr = Object.entries(products);
 
   const transformed = arr.reduce((result, value) => {
-    result.push({ id: value[0], ...value[1]  });
+    result.push({ id: value[0], ...value[1] });
     return result;
   }, []);
 
