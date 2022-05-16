fab90cb97ec81307bb8465982e650d25c0b0bbdb
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-05-16 16:44:17 -0300

    refactor(Table):  create separate TableBody component

--- front-end/src/components/molecules/Table.jsx
+++ front-end/src/components/molecules/Table.jsx
@@ -1,60 +1,28 @@
-import * as React from 'react';
+import * as React from 'react';;
 import PropTypes from 'prop-types';
 import { v4 as uuid } from 'uuid';
 
 /* Children */
-import { Button, Label } from '../atoms';
-
-/* State */
-import { useDispatch } from 'react-redux';
-import { updateProduct } from '../../redux/features/checkoutSlice';
+import { Label } from '../atoms';
+import TableBody from './TableBody';
 
 function Table({ data }) {
-  const dispatch = useDispatch();
-
-  const removeItem = (product) => {
-    dispatch(updateProduct({ ...product, quantity: 0 }));
-  };
-
   return (
     <table>
       <thead>
         <tr>
           {
-            data.header.map((heading) => {
-              return (
-                <td key={ uuid() }>
-                  <Label>{ heading }</Label>
-                </td>
-              );
-            })
+            data.header.map((heading) => (
+              <td key={ uuid() }>
+                <Label>{ heading }</Label>
+              </td>
+            ))
           }
         </tr>
       </thead>
-      <tbody>
-          {
-            data.body.map((product, index) => {
-              return (
-                <tr key={ uuid() }>
-                  <td>{ index + 1 }</td>
-                  <td>{ product.name }</td>
-                  <td>{ product.quantity }</td>
-                  <td>{ product.price }</td>
-                  <td>{ product.subTotal }</td>
-                  <td>
-                    <Button
-                      dataTestId=""
-                      type="button"
-                      handleOnClick={ () => removeItem(product) }
-                    >
-                      Remove Item
-                    </Button>
-                  </td>
-                </tr>
-              );
-            })
-          }
-      </tbody>
+
+      <TableBody data={ data.body } />
+
       <tfoot>
         <tr>
           {
---
+++ front-end/src/components/molecules/TableBody.jsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import { useDispatch } from 'react-redux'
+import { v4 as uuid } from 'uuid';
+
+/* Children */
+import { Button } from '../atoms';
+
+/* State */
+import { updateProduct } from '../../redux/features/checkoutSlice';
+
+function TableBody({ data }) {
+  const dispatch = useDispatch();
+
+  const removeItem = (product) => {
+    dispatch(updateProduct({ ...product, quantity: 0 }));
+  };
+
+  return (
+    <tbody>
+      {
+        data.map((product, index) => (
+          <tr key={ uuid() }>
+            <td>{ index + 1 }</td>
+            <td>{ product.name }</td>
+            <td>{ product.quantity }</td>
+            <td>{ product.price }</td>
+            <td>{ product.subTotal }</td>
+            <td>
+              <Button
+                dataTestId=""
+                type="button"
+                handleOnClick={ () => removeItem(product) }
+              >
+                Remove Item
+              </Button>
+            </td>
+          </tr>
+        ))
+      }
+    </tbody>
+  );
+}
+
+TableBody.propTypes = {
+  body: PropTypes.arrayOf(PropTypes.string),
+}.isRequired;
+
+export default TableBody;
