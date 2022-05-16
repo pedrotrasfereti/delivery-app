f0feb16c2f17d10d10e1d6ef6755acbb1db737e6
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-05-16 14:45:42 -0300

    feat: add checkout table to checkout page

--- front-end/src/components/pages/Checkout.jsx
+++ front-end/src/components/pages/Checkout.jsx
@@ -1,18 +1,56 @@
-import * as React from 'react';
+import React, { useEffect, useState } from 'react';
+
+/* Children */
+import Table from '../molecules/Table';
 
 /* State */
-// import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 /* Styles */
 import { ClassicLayout } from '../templates';
 
+/* Utils */
+import transformProducts from '../../utils/transformProducts';
+
 function Checkout() {
-  // const { products } = useSelector((state) => state.checkout); 
+  const initialState = {
+    header: [
+      'Item',
+      'Name',
+      'Quantity',
+      'Unit Value',
+      'Sub-total',
+      'Remove Item',
+    ],
+    body: [''],
+    // footer[0]: label, footer[1]: value
+    footer: ['Total'],
+  };
+
+  const checkout = useSelector((state) => state.checkout); 
+
+  const [tableData, setTableData] = useState(initialState);
+
+  useEffect(() => {
+    if (checkout) {
+      const { products, total } = checkout;
+      const productsArray = transformProducts(products);
+
+      setTableData((prev) => ({
+        header: [...prev.header],
+        body: productsArray,
+        footer: [...prev.footer, total],
+      }));
+    }
+  }, []);
 
   return (
     <ClassicLayout id="checkout-page">
       <main>
-        <h1>Hello World</h1>
+        <Table
+          id="checkout-table"
+          data={ tableData }
+        />
       </main>
     </ClassicLayout>
   );
