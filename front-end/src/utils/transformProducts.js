6716cb461ee9faef4b01cc3ddcea072d974d5131
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-05-16 14:40:14 -0300

    feat: add sub-total to transformed products array

--- front-end/src/utils/transformProducts.js
+++ front-end/src/utils/transformProducts.js
@@ -2,7 +2,14 @@ function transformProducts(products) {
   const arr = Object.entries(products);
 
   const transformed = arr.reduce((result, value) => {
-    result.push({ id: value[0], ...value[1] });
+    const subTotal = Number(value[1].price) * Number(value[1].quantity);
+
+    result.push({
+      id: value[0],
+      subTotal: Math.round((subTotal + Number.EPSILON) * 100) / 100,
+      ...value[1],
+  });
+
     return result;
   }, []);
 
