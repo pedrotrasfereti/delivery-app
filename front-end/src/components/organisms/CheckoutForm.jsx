4003f0f48ea05744c5b60ee7a316e22122e9abb5
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-05-16 20:26:21 -0300

    feat(CheckoutForm): implement getAllSellers service

--- front-end/src/components/organisms/CheckoutForm.jsx
+++ front-end/src/components/organisms/CheckoutForm.jsx
@@ -5,12 +5,33 @@ import { Navigate } from 'react-router-dom';
 import { Button, Fieldset } from '../atoms';
 import { TextInputLabel } from '../molecules';
 
+/* Services */
+import { getAllSellers } from '../../services/request';
+
 function CheckoutForm() {
+  const [sellers, setSellers] = useState([]);
   const [seller, setSeller] = useState('');
   const [address, setAddress] = useState('');
   const [addressNum, setAddressNum] = useState('');
   const [submitDisabled, setSubmitDisabled] = useState(true);
   const [shouldRedirect, setShouldRedirect] = useState(false);
+
+  useEffect(() => {
+    const user = localStorage.getItem('user');
+    const { token } = JSON.parse(user);
+
+    async function getData() {
+      try {
+        const sellersData = await getAllSellers(token);
+        setSeller(sellersData[0].id); // default select value
+        setSellers(sellersData);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    getData();
+  }, []);
 
   useEffect(() => {
     if (seller && address && addressNum) {
@@ -50,11 +71,11 @@ function CheckoutForm() {
           value={ seller }
           onChange={ (e) => setSeller(e.target.value) }
         >
-          <option value=""></option>
-          <option value=""></option>
-          <option value=""></option>
-          <option value=""></option>
-          <option value=""></option>
+          {
+            sellers.map(({ id, name }) => (
+              <option value={ id }>{ name }</option>
+            ))
+          }
         </select>
 
         <TextInputLabel
