32d44b3abe51b7fc68a6a0cf40de0412a2eddad1
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-05-16 17:44:23 -0300

    feat: create CheckoutForm component

---
+++ front-end/src/components/organisms/CheckoutForm.jsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+function CheckoutForm() {
+  return <form id="checkout-form" />;
+}
+
+export default CheckoutForm;
--- front-end/src/components/organisms/index.js
+++ front-end/src/components/organisms/index.js
@@ -1,3 +1,4 @@
+export { default as CheckoutForm } from './CheckoutForm';
 export { default as LoginForm } from './LoginForm';
 export { default as ProductCards } from './ProductCards';
 export { default as RegisterForm } from './RegisterForm';
