1fd48f27b22586a0ceaea623c1c6102ec1eeb5a6
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-05-17 19:40:49 -0300

    feat: create Order page component

---
+++ front-end/src/components/pages/Order.jsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+function Order() {
+  return <h1>Hello World!</h1>;
+}
+
+export default Order;
--- front-end/src/components/pages/index.js
+++ front-end/src/components/pages/index.js
@@ -1,5 +1,6 @@
 export { default as Base } from './Base';
 export { default as Checkout } from './Checkout';
 export { default as Login } from './Login';
+export { default as Order } from './Order';
 export { default as Register } from './Register';
 export { default as Products } from './Products';
