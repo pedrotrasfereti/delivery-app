d2dff24921378ae4ce6f94af2fde6107a0b56578
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-05-18 17:53:20 -0300

    refactor: checkoutSlice reducers and state

--- front-end/src/redux/features/checkoutSlice.js
+++ front-end/src/redux/features/checkoutSlice.js
@@ -1,68 +1,37 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  total: 0,
-  products: {},
-};
-
-const calcTotal = (total, price, operation = 'add') => {
-  let result = total;
-
-  if (operation === 'add') {
-    result = total + price;
-  } else if (operation === 'subtract') {
-    result = total - price;
-  }
-
-  return Math.round((result + Number.EPSILON) * 100) / 100;
+  cart: [],
+  totalPrice: 0,
 };
 
 export const checkoutSlice = createSlice({
   name: 'checkout',
   initialState,
   reducers: {
-    updateProduct: (state, action) => {
-      const { id, price, name, quantity } = action.payload;
+    updateCart: (state, action) => {
+      state.cart = action.payload;
+    },
+    updateTotalPrice: (state, action) => {
+      state.totalPrice = action.payload;
+    },
+    removeItem: (state, action) => {
+      const targetId = action.payload;
 
-      if (state.products[id]) {
-        const resetTotal = calcTotal(
-          state.total,
-          state.products[id].quantity * price,
-          'subtract',
-        );
+      const updatedCart = state.cart.filter((p) => p.id !== targetId);
 
-        if (quantity === 0) {
-          const updatedProducts = { ...state.products };
-          delete updatedProducts[id];
+      console.log(targetId);
+      console.log(updatedCart);
 
-          return {
-            total: calcTotal(resetTotal, quantity * price),
-            products: updatedProducts,
-          };
-        }
-
-        return {
-          total: calcTotal(resetTotal, quantity * price),
-          products: {
-            ...state.products,
-            [id]: { quantity, price, name },
-          },
-        };
-      }
-
-      return {
-        total: calcTotal(state.total, quantity * price),
-        products: {
-          ...state.products,
-          [id]: { quantity, price, name },
-        },
-      };
+      state.cart = updatedCart;
     },
   },
 });
 
 export const {
-  updateProduct,
+  updateCart,
+  updateTotalPrice,
+  removeItem,
 } = checkoutSlice.actions;
 
 export default checkoutSlice.reducer;
