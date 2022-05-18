5e9bdecf9f25bfac3d16a9ad38fe334365540605
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-05-18 18:40:50 -0300

    refactor(CheckoutBtn): temporarily remove styles

--- front-end/src/components/atoms/CheckoutBtn.jsx
+++ front-end/src/components/atoms/CheckoutBtn.jsx
@@ -6,59 +6,59 @@ import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 
 /* Styles */
-import { styled } from '../../stitches.config';
+// import { styled } from '../../stitches.config';
 
-export const StitchesComponent = styled('button', {
-  appearance: 'none',
-  bottom: '15px',
-  border: '0',
-  borderRadius: '$edge',
-  display: 'flex',
-  height: '50px',
-  position: 'fixed',
-  right: '15px',
-  width: '150px',
+// export const StitchesComponent = styled('button', {
+//   appearance: 'none',
+//   bottom: '15px',
+//   border: '0',
+//   borderRadius: '$edge',
+//   display: 'flex',
+//   height: '50px',
+//   position: 'fixed',
+//   right: '15px',
+//   width: '150px',
 
-  '&:hover:not(:disabled)': {
-    cursor: 'pointer',
-  },
+//   '&:hover:not(:disabled)': {
+//     cursor: 'pointer',
+//   },
 
-  '&>.customer_products__checkout-label, &>.customer_products__checkout-bottom-value': {
-    alignItems: 'center',
-    display: 'flex',
-    justifyContent: 'center',
-    height: '100%',
+//   '&>.customer_products__checkout-label, &>.customer_products__checkout-bottom-value': {
+//     alignItems: 'center',
+//     display: 'flex',
+//     justifyContent: 'center',
+//     height: '100%',
 
-    '&>span': {
-      color: '$textLight',
-      fontFamily: '$sans',
-      fontSize: '$4',
-      fontWeight: '$4',
-    },
-  },
+//     '&>span': {
+//       color: '$textLight',
+//       fontFamily: '$sans',
+//       fontSize: '$4',
+//       fontWeight: '$4',
+//     },
+//   },
 
-  '&>.customer_products__checkout-label': {
-    backgroundColor: '$primary',
-    flex: '1.15',
-  },
+//   '&>.customer_products__checkout-label': {
+//     backgroundColor: '$primary',
+//     flex: '1.15',
+//   },
 
-  '&>.customer_products__checkout-bottom-value': {
-    backgroundColor: '$accent2',
-    flex: '1',
-  },
+//   '&>.customer_products__checkout-bottom-value': {
+//     backgroundColor: '$accent2',
+//     flex: '1',
+//   },
 
-  variants: {
-    flat: {
-      true: {
-        position: 'static',
-        borderRadius: '0',
-        height: '25px',
-        right: '0',
-        width: '100%',
-      },
-    },
-  },
-});
+//   variants: {
+//     flat: {
+//       true: {
+//         position: 'static',
+//         borderRadius: '0',
+//         height: '25px',
+//         right: '0',
+//         width: '100%',
+//       },
+//     },
+//   },
+// });
 
 function CheckoutBtn({ flat }) {
   const { totalPrice } = useSelector((state) => state.checkout);
@@ -66,7 +66,14 @@ function CheckoutBtn({ flat }) {
   const [shouldRedirect, setShouldRedirect] = useState(false);
 
   return (
-    <StitchesComponent
+    // <StitchesComponent
+    //   data-testid="customer_products__button-cart"
+    //   disabled={ !totalPrice }
+    //   onClick={ () => setShouldRedirect(true) }
+    //   flat={ flat }
+    // >
+    <button
+      type="button"
       data-testid="customer_products__button-cart"
       disabled={ !totalPrice }
       onClick={ () => setShouldRedirect(true) }
@@ -87,7 +94,8 @@ function CheckoutBtn({ flat }) {
       {
         shouldRedirect && <Navigate replace to="/customer/checkout" />
       }
-    </StitchesComponent>
+    </button>
+    // </StitchesComponent>
   );
 }
 
