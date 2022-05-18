faa84490fb0d7319655b3f99e8bc5cbe7eb7cdf2
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-05-18 18:40:26 -0300

    refactor(ProductCard): temporarily remove styles

--- front-end/src/components/molecules/ProductCard.jsx
+++ front-end/src/components/molecules/ProductCard.jsx
@@ -9,124 +9,124 @@ import { updateProductQty } from '../../redux/features/productsSlice';
 import Control from '../atoms/Control';
 
 /* Styles */
-import { styled } from '../../stitches.config';
+// import { styled } from '../../stitches.config';
 
-const StitchesComponent = styled('div', {
-  backgroundColor: '$loContrast',
-  border: '0',
-  borderRadius: '$default',
-  boxShadow: 'rgba(149, 157, 165, 0.2) 0 8px 20px',
-  display: 'flex',
-  flexFlow: 'column nowrap',
-  gap: '$3',
-  height: '350px',
-  width: '260px',
+// const StitchesComponent = styled('div', {
+//   backgroundColor: '$loContrast',
+//   border: '0',
+//   borderRadius: '$default',
+//   boxShadow: 'rgba(149, 157, 165, 0.2) 0 8px 20px',
+//   display: 'flex',
+//   flexFlow: 'column nowrap',
+//   gap: '$3',
+//   height: '350px',
+//   width: '260px',
 
-  '&>.product-card__image': {
-    flex: '1.5',
-    position: 'relative',
-    overflow: 'hidden',
+//   '&>.product-card__image': {
+//     flex: '1.5',
+//     position: 'relative',
+//     overflow: 'hidden',
 
-    '&>img': {
-      width: '100%',
-      height: '100%',
-      objectFit: 'contain',
-    },
+//     '&>img': {
+//       width: '100%',
+//       height: '100%',
+//       objectFit: 'contain',
+//     },
 
-    '&>.product-card__discount:not(:empty)': {
-      alignItems: 'center',
-      color: '$textLight',
-      backgroundColor: 'rgb(3,109,85)',
-      background:
-        'linear-gradient(333deg, rgba(3,109,85,1) 40%, rgba(61,149,105,1) 100%)',
-      borderRadius: '0 $default',
-      display: 'flex',
-      fontFamily: '$sans',
-      fontSize: '$2',
-      fontWeight: '$5',
-      height: '25px',
-      justifyContent: 'center',
-      opacity: '.9',
-      padding: '$3 $2',
-      position: 'absolute',
-      right: '0',
-      top: '0',
-    },
-  },
+//     '&>.product-card__discount:not(:empty)': {
+//       alignItems: 'center',
+//       color: '$textLight',
+//       backgroundColor: 'rgb(3,109,85)',
+//       background:
+//         'linear-gradient(333deg, rgba(3,109,85,1) 40%, rgba(61,149,105,1) 100%)',
+//       borderRadius: '0 $default',
+//       display: 'flex',
+//       fontFamily: '$sans',
+//       fontSize: '$2',
+//       fontWeight: '$5',
+//       height: '25px',
+//       justifyContent: 'center',
+//       opacity: '.9',
+//       padding: '$3 $2',
+//       position: 'absolute',
+//       right: '0',
+//       top: '0',
+//     },
+//   },
 
-  '&>.product-card__content': {
-    display: 'flex',
-    flex: '1',
-    flexFlow: 'column nowrap',
-    gap: '$2',
-    padding: '$3',
+//   '&>.product-card__content': {
+//     display: 'flex',
+//     flex: '1',
+//     flexFlow: 'column nowrap',
+//     gap: '$2',
+//     padding: '$3',
 
-    '&>h3': {
-      color: '$textDark',
-      fontFamily: '$sans',
-      fontSize: '$4',
-      fontWeight: '$5',
-    },
+//     '&>h3': {
+//       color: '$textDark',
+//       fontFamily: '$sans',
+//       fontSize: '$4',
+//       fontWeight: '$5',
+//     },
 
-    '&>p': {
-      color: '$gray600',
-      fontFamily: '$sans',
-      fontSize: '$3',
-      fontWeight: '$4',
-    },
+//     '&>p': {
+//       color: '$gray600',
+//       fontFamily: '$sans',
+//       fontSize: '$3',
+//       fontWeight: '$4',
+//     },
 
-    '&>.product-card__content-bottom': {
-      alignItems: 'center',
-      display: 'flex',
-      justifyContent: 'space-between',
-      marginTop: 'auto',
+//     '&>.product-card__content-bottom': {
+//       alignItems: 'center',
+//       display: 'flex',
+//       justifyContent: 'space-between',
+//       marginTop: 'auto',
 
-      '&>.product-card__content-price': {
-        color: '$accent1',
-        fontFamily: '$sans',
-        fontSize: '$4',
-        fontWeight: '$5',
-      },
+//       '&>.product-card__content-price': {
+//         color: '$accent1',
+//         fontFamily: '$sans',
+//         fontSize: '$4',
+//         fontWeight: '$5',
+//       },
 
-      '&>.product-card__content-controls': {
-        display: 'flex',
+//       '&>.product-card__content-controls': {
+//         display: 'flex',
 
-        '&>.product-card__product-quantity': {
-          backgroundColor: '$background',
-          color: '$textDark',
-          fontFamily: '$sans',
-          fontSize: '$3',
-          fontWeight: '$5',
-          outline: '0',
-          textAlign: 'center',
-          width: '$4',
-        },
-      },
-    },
-  },
+//         '&>.product-card__product-quantity': {
+//           backgroundColor: '$background',
+//           color: '$textDark',
+//           fontFamily: '$sans',
+//           fontSize: '$3',
+//           fontWeight: '$5',
+//           outline: '0',
+//           textAlign: 'center',
+//           width: '$4',
+//         },
+//       },
+//     },
+//   },
 
-  '@bp3': {
-    height: '300px',
-    width: '210px',
+//   '@bp3': {
+//     height: '300px',
+//     width: '210px',
 
-    '&>.product-card__content': {
-      '&>p': {
-        fontSize: '$2',
-      },
-    },
-  },
+//     '&>.product-card__content': {
+//       '&>p': {
+//         fontSize: '$2',
+//       },
+//     },
+//   },
 
-  '@bp2': {
-    height: '265px',
-    width: '175px',
+//   '@bp2': {
+//     height: '265px',
+//     width: '175px',
 
-    '&>.product-card__content': {
-      '&>h3': {
-        fontSize: '$3',
-      },
-    },
-  },
-});
+//     '&>.product-card__content': {
+//       '&>h3': {
+//         fontSize: '$3',
+//       },
+//     },
+//   },
+// });
 
 function ProductCard({ product }) {
   const { id, urlImage, name, price, quantity } = product;
@@ -149,7 +149,8 @@ function ProductCard({ product }) {
   };
 
   return (
-    <StitchesComponent>
+    // <StitchesComponent>
+    <div>
       <div className="product-card__image">
         <img
           data-testid={
@@ -211,7 +212,8 @@ function ProductCard({ product }) {
           </div>
         </div>
       </div>
-    </StitchesComponent>
+    </div>
+    // </StitchesComponent>
   );
 }
 
