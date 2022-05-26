696e866b1a66e4495d18bd154e1e657ca142c5f7
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-05-26 19:49:54 -0300

    fix(LoginForm): add missing import

--- front-end/src/components/organisms/LoginForm.jsx
+++ front-end/src/components/organisms/LoginForm.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 /* Children */
 import BaseForm from './BaseForm';
