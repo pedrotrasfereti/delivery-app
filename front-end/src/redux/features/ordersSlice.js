560361a9e38d08fd17409f55187cf01cdd1c968b
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-05-26 18:52:35 -0300

    feat(redux): create setOrders reducer + fetchOrders and mark thunks

--- front-end/src/redux/features/ordersSlice.js
+++ front-end/src/redux/features/ordersSlice.js
@@ -1,4 +1,57 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+/* Services */
+import {
+  getAllOrders,
+  updateOrder,
+} from '../../services/request';
+
+export const fetchOrders = createAsyncThunk(
+  'orders/fetchOrders',
+  async (payload) => getAllOrders(payload.token, payload.role),
+);
+
+export const markAsDelivered = createAsyncThunk(
+  'orders/markAsDelivered',
+  async (payload) => {
+    const data = await updateOrder(
+      payload.token,
+      payload.role,
+      payload.orderId,
+      { status: 'Entregue' },
+    );
+
+    return data;
+  },
+);
+
+export const markAsDispatched = createAsyncThunk(
+  'orders/markAsDispatched',
+  async (payload) => {
+    const data = await updateOrder(
+      payload.token,
+      payload.role,
+      payload.orderId,
+      { status: 'Em Trânsito' },
+    );
+
+    return data;
+  },
+);
+
+export const markAsPreparing = createAsyncThunk(
+  'orders/markAsPreparing',
+  async (payload) => {
+    const data = await updateOrder(
+      payload.token,
+      payload.role,
+      payload.orderId,
+      { status: 'Preparando' },
+    );
+
+    return data;
+  },
+);
 
 const initialState = {
   orders: [],
@@ -8,12 +61,58 @@ export const ordersSlice = createSlice({
   name: 'orders',
   initialState,
   reducers: {
-    addOrder: (state, action) => {
-      state.orders.push(action.payload);
+    setOrders: (state, action) => {
+      state.orders = action.payload;
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchOrders.fulfilled, (state, action) => {
+      const data = action.payload;
+
+      const orders = data.map((o) => ({
+        ...o,
+        products: o.products.map(({ urlImage, ...p }) => {
+          const subTotal = Math.round(
+            (p.price * p.quantity.quantity + Number.EPSILON) * 100,
+          ) / 100;
+
+          return {
+            ...p,
+            subTotal,
+            quantity: p.quantity.quantity,
+          };
+        }),
+      }));
+
+      state.orders = orders;
+    });
+
+    builder.addCase(markAsDelivered.fulfilled, (state, action) => {
+      const { id: orderId } = action.payload;
+
+      const targetId = state.orders.findIndex((o) => o.id === orderId);
+
+      state.orders[targetId].status = 'Entregue';
+    });
+
+    builder.addCase(markAsPreparing.fulfilled, (state, action) => {
+      const { id: orderId } = action.payload;
+
+      const targetId = state.orders.findIndex((o) => o.id === orderId);
+
+      state.orders[targetId].status = 'Preparando';
+    });
+
+    builder.addCase(markAsDispatched.fulfilled, (state, action) => {
+      const { id: orderId } = action.payload;
+
+      const targetId = state.orders.findIndex((o) => o.id === orderId);
+
+      state.orders[targetId].status = 'Em Trânsito';
+    });
   },
 });
 
-export const { addOrder } = ordersSlice.actions;
+export const { setOrders } = ordersSlice.actions;
 
 export default ordersSlice.reducer;
