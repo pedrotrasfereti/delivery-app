60611e35f7228045541c3b5401b8f3883bbb1de5
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-05-26 19:15:47 -0300

    feat(utils): create formatFloat util

---
+++ front-end/src/utils/formatFloat.js
@@ -0,0 +1,9 @@
+/**
+ * Rounds a float to two decimal places and replaces
+ * period with comma mark.
+
+ * @param value The initial value.
+ */
+export default function formatFloat(value) {
+  return String(Number(value).toFixed(2)).replace('.', ',');
+}
