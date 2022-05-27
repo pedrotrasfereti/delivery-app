eb45b0afbd417408b5bf68c748c40eaaddbc2a33
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-05-27 14:43:20 -0300

    refactor(Checkout): remove unnecessary React hooks

--- front-end/src/components/pages/Checkout.jsx
+++ front-end/src/components/pages/Checkout.jsx
@@ -1,4 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+
+/* State */
 import { useSelector } from 'react-redux';
 
 /* Children */
@@ -11,36 +13,23 @@ import { ClassicLayout } from '../templates';
 function Checkout() {
   const { cart, totalPrice } = useSelector((state) => state.checkout);
 
-  const initialState = {
-    header: [
-      'Item',
-      'Name',
-      'Quantity',
-      'Unit Value',
-      'Sub-total',
-      'Remove Item',
-    ],
-    body: cart,
-    // footer[0]: label, footer[1]: value
-    footer: ['Total', totalPrice.toFixed(2)],
-  };
-
-  const [tableData, setTableData] = useState(initialState);
-
-  useEffect(() => {
-    setTableData((prev) => ({
-      ...prev,
-      body: cart,
-      footer: [prev.footer[0], totalPrice.toFixed(2)],
-    }));
-  }, [cart, totalPrice]);
-
   return (
     <ClassicLayout id="checkout-page">
       <main>
         <Table
           id="checkout-table"
-          data={ tableData }
+          data={ {
+            header: [
+              'Item',
+              'Name',
+              'Quantity',
+              'Unit Value',
+              'Sub-total',
+              'Remove Item',
+            ],
+            body: cart,
+            footer: ['Total', totalPrice],
+          } }
         />
 
         <CheckoutForm />
