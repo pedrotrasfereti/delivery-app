d936ad4b115e1812be7e89b9be425eae81e5243c
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-05-29 14:46:55 -0300

    refactor: rename icon asset

--- front-end/src/assets/index.js
+++ front-end/src/assets/index.js
@@ -1,3 +1,4 @@
+export { default as Chevron } from './chevron.png';
 export { default as DeliveryMan } from './deliveryman.jpg';
 export { default as Logo } from './logo.png';
 export { default as Packages } from './packages.jpg';
