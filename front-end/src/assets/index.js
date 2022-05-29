ebf5ccf07f33c1aeb29d49130bb5940d018869f9
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-05-29 15:09:30 -0300

    feat: add icon asset

--- front-end/src/assets/index.js
+++ front-end/src/assets/index.js
@@ -1,4 +1,5 @@
-export { default as Chevron } from './chevron.png';
 export { default as DeliveryMan } from './deliveryman.jpg';
+export { default as ExpandLess } from './chevron_up.png';
+export { default as ExpandMore } from './chevron.png';
 export { default as Logo } from './logo.png';
 export { default as Packages } from './packages.jpg';
