72128568c47d089e26582eda92964df7a4b627f4
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-05-29 15:16:37 -0300

    feat: resize Table component

--- front-end/src/components/molecules/Table.jsx
+++ front-end/src/components/molecules/Table.jsx
@@ -17,7 +17,7 @@ const StitchesComponent = styled('table', {
   boxShadow:
     'rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px',
   color: '$textDark',
-  width: '70rem',
+  width: '65rem',
 
   '& caption': {
     backgroundColor: '$loContrast',
