d277c0677fa874246471517f79710650116a335b
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-05-29 15:09:40 -0300

    feat: create Select component

--- front-end/src/components/atoms/Select.jsx
+++ front-end/src/components/atoms/Select.jsx
@@ -1,17 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+/* Assets */
+import { ExpandLess, ExpandMore } from '../../assets';
+
 /* Styles */
 import { styled } from '../../stitches.config';
 
 const StitchesComponent = styled('select', {
-  background: '$loContrast',
+  appearance: 'none',
+  backgroundColor: '$loContrast',
+  backgroundImage: `url(${ExpandMore})`,
+  backgroundPosition: '95% 50%',
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: '25px',
   border: '0',
   borderRadius: '$default',
-  padding: '$3',
   boxShadow:
-    'rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px',
+    'rgba(0, 0, 0, 0.16) 0px 1px 4px',
+  color: '$textDark',
+  fontFamily: '$sans',
+  fontSize: '$3',
+  fontWeight: '$4',
+  padding: '$3',
   width: '17rem',
+  '-moz-appearance': 'none', /* Firefox */
+  '-webkit-appearance': 'none', /* Safari and Chrome */
+
+  '&:focus': {
+    boxShadow:
+      '0 0 0 .15rem rgba(5, 108, 249, .5)',
+    backgroundImage: `url(${ExpandLess})`,
+  },
 });
 
 export default function Select({ name, value, handleOnChange, children }) {
