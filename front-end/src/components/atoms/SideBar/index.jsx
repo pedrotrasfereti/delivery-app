be299f9a43c840314a3367d203e3459b5f65a3a3
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-05-30 17:57:36 -0300

    fix(SideBar): replace SimpleBar with 'overflowX:scroll'

--- front-end/src/components/atoms/SideBar/index.jsx
+++ front-end/src/components/atoms/SideBar/index.jsx
@@ -1,55 +1,51 @@
 import React from 'react';
-import 'simplebar/dist/simplebar.min.css';
 
 /* Children */
-import SimpleBar from 'simplebar-react';
 import { BsArrowUp as ArrowUp } from 'react-icons/bs';
-import SideBarStitches from './SideBarStitches';
 
-function SideBar() {
+/* Styles */
+import Styled from './Styled';
+
+export default function SideBar() {
   return (
-    <SideBarStitches>
+    <Styled>
       <header>
         <h3>Categories</h3>
         <ArrowUp className="Icon" />
       </header>
 
-      <SimpleBar scrollbarMaxSize={ 65 }>
-        <ul className="Categories">
-          <li id="beverages" className="Selected">
-            <span>Beverages</span>
-          </li>
-          <li id="dessert">
-            <span>Dessert</span>
-          </li>
-          <li id="fast-food">
-            <span>Fast Food</span>
-          </li>
-          <li id="healthy">
-            <span>Healthy</span>
-          </li>
-          <li id="iced-coffee">
-            <span>Iced Coffee</span>
-          </li>
-          <li id="mexican">
-            <span>Mexican</span>
-          </li>
-          <li id="milkshake">
-            <span>Milkshake</span>
-          </li>
-          <li id="sushi">
-            <span>Sushi</span>
-          </li>
-          <li id="thai">
-            <span>Thai</span>
-          </li>
-          <li id="veggie">
-            <span>Veggie</span>
-          </li>
-        </ul>
-      </SimpleBar>
-    </SideBarStitches>
+      <ul className="Categories">
+        <li id="beverages" className="Selected">
+          <span>Beverages</span>
+        </li>
+        <li id="dessert">
+          <span>Dessert</span>
+        </li>
+        <li id="fast-food">
+          <span>Fast Food</span>
+        </li>
+        <li id="healthy">
+          <span>Healthy</span>
+        </li>
+        <li id="iced-coffee">
+          <span>Iced Coffee</span>
+        </li>
+        <li id="mexican">
+          <span>Mexican</span>
+        </li>
+        <li id="milkshake">
+          <span>Milkshake</span>
+        </li>
+        <li id="sushi">
+          <span>Sushi</span>
+        </li>
+        <li id="thai">
+          <span>Thai</span>
+        </li>
+        <li id="veggie">
+          <span>Veggie</span>
+        </li>
+      </ul>
+    </Styled>
   );
 }
-
-export default SideBar;
