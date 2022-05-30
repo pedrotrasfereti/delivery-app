c86152b751bd08ca7d849fb15c64ef61ac6c1806
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-05-30 13:13:07 -0300

    feat: create Account route

--- front-end/src/routes/index.js
+++ front-end/src/routes/index.js
@@ -12,13 +12,14 @@ import { NavBar } from '../components/atoms';
 import { Header } from '../components/molecules';
 
 import {
+  Account,
   Checkout,
   Login,
   Order,
+  Orders,
   Products,
   Register,
 } from '../components/pages';
-import Orders from '../components/pages/Orders';
 
 function AppRoutes() {
   return (
@@ -31,6 +32,7 @@ function AppRoutes() {
         <Route path="/customer/orders" element={ <Orders /> } />
         <Route path="/customer/checkout" element={ <Checkout /> } />
         <Route path="/customer/products" element={ <Products /> } />
+        <Route path="/account" element={ <Account /> } />
         <Route path="/register" element={ <Register /> } />
         <Route path="/login" element={ <Login /> } />
         <Route path="/" element={ <Navigate replace to="/login" /> } />
