61de45f28401992b9730ee6deed47dac33774bc6
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-05-30 19:18:45 -0300

    feat: create OrderCards component

--- front-end/src/components/organisms/index.js
+++ front-end/src/components/organisms/index.js
@@ -1,4 +1,5 @@
 export { default as CheckoutForm } from './CheckoutForm';
 export { default as LoginForm } from './LoginForm';
 export { default as ProductCards } from './ProductCards';
+export { default as OrderCards } from './OrderCards';
 export { default as RegisterForm } from './RegisterForm';
