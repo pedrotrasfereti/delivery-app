b3eb70340c5748fa2742dd0f1ad98f9e2a8958d8
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-05-31 14:05:28 -0300

    refactor(Header): split React and Stitches components

---
+++ front-end/src/components/molecules/Header/Styled.js
@@ -0,0 +1,43 @@
+import { styled } from '../../../stitches.config';
+
+/* Children */
+import StyledNavBar from '../../atoms/NavBar/Styled';
+// import StyledCheckoutBtn from '../../atoms/CheckoutBtn/Styled';
+
+export default styled('header', {
+  alignItems: 'center',
+  backgroundColor: '$loContrast',
+  boxShadow: 'rgba(149, 157, 165, 0.2) 0 1px 1px',
+  display: 'flex',
+  height: '$headerHeight',
+  position: 'fixed',
+  width: '100%',
+  zIndex: '99',
+
+  '&>.Container': {
+    display: 'flex',
+    alignItems: 'center',
+    gap: '$2',
+
+    '& img': {
+      width: '42.5px',
+    },
+
+    '& h1': {
+      color: '$textDark',
+      fontFamily: '$sans2',
+      fontSize: '$5',
+      fontWeight: '$4',
+    },
+  },
+
+  '& *': {
+    margin: '0 auto',
+  },
+
+  '@bp3': {
+    [`& ${StyledNavBar}`]: {
+      display: 'none',
+    },
+  },
+});
---
+++ front-end/src/components/molecules/Header/index.jsx
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import { useLocation } from 'react-router-dom';
+
+/* Assets */
+import { Logo } from '../../../assets';
+
+/* Children */
+import { NavBar, CheckoutBtn } from '../../atoms';
+
+/* Styles */
+import Styled from './Styled';
+
+function Header() {
+  const { pathname } = useLocation();
+
+  const excludeRoutes = [
+    '/',
+    '/login',
+    '/register',
+  ];
+
+  const shouldRender = !excludeRoutes.includes(pathname);
+
+  return shouldRender && (
+    <Styled>
+      <button
+        type="button"
+        className="BackButton"
+      >
+        X
+      </button>
+
+      <div className="Container">
+        <img src={ Logo } alt="Deliveree" />
+        <h1>Deliveree</h1>
+      </div>
+
+      <NavBar />
+
+      <CheckoutBtn />
+    </Styled>
+  );
+}
+
+export default Header;
