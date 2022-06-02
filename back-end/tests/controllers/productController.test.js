67f05b4c077b34b1a5cfcd32ca1a626e917cd3ba
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-06-02 19:26:32 -0300

    refactor: scrap vercel.json and move folders out of /src

--- back-end/tests/controllers/productController.test.js
+++ back-end/tests/controllers/productController.test.js
@@ -1,20 +1,19 @@
-const sinon = require("sinon");
+const sinon = require('sinon');
 const { expect } = require('chai');
-const productService = require("../../app/services/productService");
-const productController = require("../../app/controllers/productController");
-const { productsMock } = require("../mocks/Database");
+const productService = require('../../app/services/productService');
+const productController = require('../../app/controllers/productController');
+const { productsMock } = require('../mocks/Database');
 
-describe('Tests productController', () => {
-  afterEach(() => {
+describe('Tests productController', function () {
+  afterEach(function () {
     sinon.restore();
   });
 
-
-  describe('Tests getAll method', () => {
+  describe('Tests getAll method', function () {
     const res = {};
     const req = {};
 
-    it('Return status 200 with the products', async () => {
+    it('Return status 200 with the products', async function () {
       const getAllStub = sinon.stub(productService, 'getAll').resolves(productsMock);
       res.status = sinon.stub().returns(res);
       res.json = sinon.stub().returns(null);
@@ -23,6 +22,6 @@ describe('Tests productController', () => {
       sinon.assert.calledWith(getAllStub, { raw: true });
       expect((res.status).calledWith(200)).to.equal(true);
       expect((res.json).calledWith(productsMock)).to.equal(true);
-    })
-  })
-})
+    });
+  });
+});
