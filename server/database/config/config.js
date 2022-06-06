1270eb49d8ef674ff09f9fa02d8ec6989f0e3544
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-06-06 18:02:24 -0300

    feat: set cleardb variables in database config

--- server/database/config/config.js
+++ server/database/config/config.js
@@ -1,20 +1,21 @@
 require('dotenv').config();
 
-const environment = process.env.NODE_ENV || 'test';
+// const environment = process.env.NODE_ENV || 'test';
 
-const suffix = {
-  prod: '',
-  production: '',
-  dev: '-dev',
-  development: '-dev',
-  test: '-test',
-};
+// const suffix = {
+//   prod: '',
+//   production: '',
+//   dev: '-dev',
+//   development: '-dev',
+//   test: '-test',
+// };
 
 const options = {
-  host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'localhost',
+  host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'us-cdbr-east-05.cleardb.net',
   port: process.env.MYSQL_PORT || '3306',
-  database: 
-    `${process.env.MYSQL_DB_NAME || 'delivery-app'}${suffix[environment] || suffix.test}`,
+  // database: 
+  //   `${process.env.MYSQL_DB_NAME || 'delivery-app'}${suffix[environment] || suffix.test}`,
+  database: 'heroku_505f68b2a2b6b45',
   username: process.env.MYSQL_USER || 'root',
   password: process.env.MYSQL_PASSWORD || 'password',
   dialect: 'mysql',
