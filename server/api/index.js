256089aacb0bc8797be250d9c9d47287ec8f03ae
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-06-06 19:18:39 -0300

    fix: set correct 'images' static route

--- server/api/index.js
+++ server/api/index.js
@@ -14,10 +14,10 @@ const api = express();
 // middlewares
 api.use(cors());
 api.use(express.json());
-api.use('/images', express.static(path.join(__dirname, '../../public')));
 
 // routes
 api.get('/', (_req, res) => res.send('Delivery App API!'));
+api.use('/images', express.static(path.join(__dirname, '../public')));
 api.use('/register', userRouter);
 api.use('/login', loginRouter);
 api.use('/customer', validateUserToken('customer'), customerRouter);
