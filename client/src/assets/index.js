f62899dbd659851c5ba639841b936249e04cc100
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-06-06 20:06:17 -0300

    perf(client): convert photos to webp format

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,6 +1,6 @@
 export { default as BlankProfile } from './blank_profile.png';
-export { default as DeliveryMan } from './delivery_man.jpg';
+export { default as DeliveryMan } from './delivery_man.webp';
 export { default as ExpandLess } from './chevron_up.png';
 export { default as ExpandMore } from './chevron.png';
 export { default as Logo } from './logo.png';
-export { default as Packages } from './packages.jpg';
+export { default as Packages } from './packages.webp';
