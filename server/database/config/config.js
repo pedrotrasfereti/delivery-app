b7f4832c912154c100c04f25abe46faa634c2e60
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-06-07 12:11:27 -0300

    refactor: set database variables for dev environment

--- server/database/config/config.js
+++ server/database/config/config.js
@@ -1,23 +1,22 @@
 require('dotenv').config();
 
-// const environment = process.env.NODE_ENV || 'test';
+const environment = process.env.NODE_ENV || 'test';
 
-// const suffix = {
-//   prod: '',
-//   production: '',
-//   dev: '-dev',
-//   development: '-dev',
-//   test: '-test',
-// };
+const suffix = {
+  prod: '',
+  production: '',
+  dev: '-dev',
+  development: '-dev',
+  test: '-test',
+};
 
 const options = {
-  host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'us-cdbr-east-05.cleardb.net',
+  host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'localhost',
   port: process.env.MYSQL_PORT || '3306',
-  // database: 
-  //   `${process.env.MYSQL_DB_NAME || 'delivery-app'}${suffix[environment] || suffix.test}`,
-  database: 'heroku_505f68b2a2b6b45',
-  username: process.env.MYSQL_USER || 'b23dcc0e8f1d81',
-  password: process.env.MYSQL_PASSWORD || '21404c14',
+  database: 
+    `${process.env.MYSQL_DB_NAME || 'delivery-app'}${suffix[environment] || suffix.test}`,
+  username: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || 'password',
   dialect: 'mysql',
   dialectOptions: {
     timezone: 'Z',
