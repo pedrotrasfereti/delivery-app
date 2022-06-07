4797454fc8615bd6404d6938fbaff4145566e93c
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-06-07 12:34:47 -0300

    refactor(server): get db config options from process.env

--- server/database/config/config.js
+++ server/database/config/config.js
@@ -1,23 +1,11 @@
 require('dotenv').config();
 
-// const environment = process.env.NODE_ENV || 'test';
-
-// const suffix = {
-//   prod: '',
-//   production: '',
-//   dev: '-dev',
-//   development: '-dev',
-//   test: '-test',
-// };
-
 const options = {
-  host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'us-cdbr-east-05.cleardb.net',
-  port: process.env.MYSQL_PORT || '3306',
-  // database: 
-  //   `${process.env.MYSQL_DB_NAME || 'delivery-app'}${suffix[environment] || suffix.test}`,
-  database: 'heroku_505f68b2a2b6b45',
-  username: process.env.MYSQL_USER || 'b23dcc0e8f1d81',
-  password: process.env.MYSQL_PASSWORD || '21404c14',
+  host: process.env.HOSTNAME || process.env.HOST,
+  port: process.env.PORT || '3306',
+  database: process.env.DB_NAME,
+  username: process.env.USER,
+  password: process.env.PASSWORD,
   dialect: 'mysql',
   dialectOptions: {
     timezone: 'Z',
