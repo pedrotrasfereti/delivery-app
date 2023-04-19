49abea80d0e23d0f107ab132d22822640b876d05
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-04-19 16:56:16 -0300

    feat: success message box styled and component

---
+++ client/src/components/molecules/SuccessMessageBox/Styled.js
@@ -0,0 +1,8 @@
+import { styled } from '../../../stitches.config';
+
+export default styled('div', {
+  backgroundColor: '$success',
+  borderRadius: '$edge',
+  display: 'flex',
+  padding: '$1 $2',
+});
