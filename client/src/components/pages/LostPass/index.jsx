08ef6ef050fa2ac09e26a4e36533619fab7aafae
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-04-19 16:48:45 -0300

    refactor: move link button to form

--- client/src/components/pages/LostPass/index.jsx
+++ client/src/components/pages/LostPass/index.jsx
@@ -4,12 +4,11 @@ import React from 'react';
 import { LostPass as LostPassImage } from '../../../assets';
 
 /* Children */
-import { Button,  HorizontalRule,  ResetPassForm, Teaser } from '../../atoms';
+import { ResetPassForm, Teaser } from '../../atoms';
 
 /* Styles */
 import { DuoGeneric } from '../../templates';
 import { ContentWrapper, DecorOne, DecorTwo } from './Styled';
-import { Link } from 'react-router-dom';
 
 const LostPass = () => {
   return (
@@ -20,19 +19,6 @@ const LostPass = () => {
           <h2>Let's change your password:</h2>
         </div>
         <ResetPassForm />
-
-        <HorizontalRule />
-
-        <span className="form__link-message">
-          Already have an account?
-          <Button
-            id="login-btn"
-            type="button"
-            link
-          >
-            <Link to="/login">Sign in</Link>
-          </Button>
-        </span>
       </ContentWrapper>
 
       <Teaser image={LostPassImage}>
