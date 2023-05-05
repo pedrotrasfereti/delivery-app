7a089fd742ddb614ba12948183b70744cf0d750c
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-05-05 13:33:07 -0300

    feat: Hook UseFetch

---
+++ client/src/hooks/UseFetch.js
@@ -0,0 +1,37 @@
+import { useCallback, useState } from 'react';
+
+const useFetch = () => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const request = useCallback(async (requestFunc, body) => {
+    let response;
+    let json;
+
+    try {
+      setError(null);
+      setLoading(true);
+      response = await requestFunc(body);
+      json = await response.json();
+
+      if (response.ok === false) throw new Error(json.message);
+    } catch (err) {
+      json = null;
+      setError(err.message);
+    } finally {
+      setData(json);
+      setLoading(false);
+      return {response, json}
+    }
+  }, [])
+
+  return {
+    data,
+    loading,
+    error,
+    request,
+  }
+};
+
+export default useFetch;
